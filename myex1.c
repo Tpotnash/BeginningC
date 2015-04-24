d567a2e49cec81cb57914e5ad2dcd67d88448cfd
Joshua Patterson <jleegippies@gmail.com>
2015-04-23 22:08:59 -0500

    first commit

---
+++ myex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int x = 0;
+	printf("Enter a number: ");
+	scanf("%d", &x);
+	printf("Here is the number you entered: %d\n", x);
+
+
+	return 0;
+}
